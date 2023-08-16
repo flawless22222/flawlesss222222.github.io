2ec737c0c0c3d6596437bdc90d0a684832a21c41
flawless22222 <140437930+flawless22222@users.noreply.github.com>
2023-08-16 19:00:02 +0300

    Update cafe.js

--- js/cafe.js
+++ js/cafe.js
@@ -304,6 +304,10 @@
         success: function(result) {
           onCallback && onCallback(result);
         },
+        error: function(xhr) {
+          onCallback && onCallback({error: 'Server error'});
+        }
+        },
       });
     }
   };
